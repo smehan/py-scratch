645060ac1bad59b5ef7ac421fff5dc7344cd4c37
Shawn Mehan <shanw.mehan@ieee.org>
2015-09-04 17:35:26 -0700

    Brute force method with hardwire modulo, that is much more memory efficient than initial naive method. Naive can not scale past 1..19. There is also a pythonic implementation of lcm(a,b) = a*b/gcd(a,b) that screams.

--- Euler/E5.py
+++ Euler/E5.py
@@ -8,7 +8,7 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 def test_divs(number, start, stop):
     no_remainder = [True]
-    for n in range (start, stop):
+    for n in range(start, stop):
         if number % n == 0:
             no_remainder.append(True)
         else:
@@ -19,12 +19,32 @@ def test_divs(number, start, stop):
         return True
 
 
+def get_smallest2(step):
+    i = step
+    while (i % 2 != 0 or i % 3 != 0 or i % 4 != 0 or i % 5 != 0 or
+         i % 6 != 0 or i % 7 != 0 or i % 8 != 0 or i % 9 != 0 or
+         i % 10 != 0 or i % 11 != 0 or i % 12 != 0 or i % 13 != 0 or
+         i % 14 != 0 or i % 15 != 0 or i % 16 != 0 or i % 17 != 0 or
+         i % 18 != 0 or i % 19 != 0 or i % 20 != 0):
+        i += step
+    return i
+
+
 def get_smallest(start, stop, ceiling):
     for s in range(1, ceiling):
-        if test_divs(s, start, stop):
+        if test_divs2(s, start, stop):
             return s
+        elif s % 100000 == 0:
+            print "...{:d}...".format(s)
         else:
             continue
 
 
-print get_smallest(1,20,100000000000)
+def get_smallest3():
+    """ And then we have this, which is amazingly efficient and can scale much higher than
+            the naive implementation
+    """
+    from fractions import gcd
+    return reduce(lambda x, y: x*y//gcd(x, y), range(1, 101))
+
+print get_smallest3()
