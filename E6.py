cea71f979d93024d30affcc27ca56cfa97350595
Shawn Mehan <shanw.mehan@ieee.org>
2015-09-04 17:58:03 -0700

    Added analytic solution for S^2_n

--- E6.py
+++ E6.py
@@ -4,10 +4,11 @@ __author__ = 'shawnmehan'
 
 """ geometric sum (1 + 2 + ... + N) = N*(N+1)/2 """
 
+
 def get_square_sum(n):
-    sum = n*(n+1)/2
-    square_sum = sum**2
-    return square_sum
+    sum = (n*(n+1)/2)**2
+    return sum
+
 
 def get_sum_squares(n):
     sum_squares = 0
@@ -15,6 +16,13 @@ def get_sum_squares(n):
       sum_squares += i**2
     return sum_squares
 
-n = 10000000
+
+def get_sum_squares2(n):
+    sum = n*(n+1)*(2*n+1)/6
+    return sum
+
+
+
+n = 100000000
 print "Difference for N=", n, " is :", get_square_sum(n) - get_sum_squares(n)
 
