889ef3613b69414a2214df51dfdf08ba2a9ba1c7
Shawn Mehan <shanw.mehan@ieee.org>
2015-09-21 18:02:19 -0700

    More carving of divisor search space.

--- Euler/E12.py
+++ Euler/E12.py
@@ -3,11 +3,6 @@ __author__ = 'shawnmehan'
 """Class to calculte the triangle numbers and then determine how many divisors they have.
 Objective is to find first triangle number with > 500 divisors"""
 
-from fractions import gcd
-
-def get_sum_next_tri_num(n, s):
-    s += n
-    return s
 
 def get_divisors(n):
     divisors = []
@@ -24,14 +19,31 @@ def get_divisors(n):
     return sorted(divisors)
 
 
+def get_divisors2(n):
+    divisors = []
+    for d in range(1, n):
+        """If d is in the list, we have crossed the half-way point and can stop"""
+        if d in divisors:
+            break
+        """Otherwise, test to see if a divisor, and then add both to set"""
+        if n % d == 0:
+            r = n / d
+            divisors.append(d)
+            divisors.append(r)
+    return sorted(divisors)
+
+
 tri_sum = 0
-for i in range(4000):
+tri_sum_divs = 0
+for i in range(6000):
     tri_sum += i+1
-    tri_sum_divs = get_divisors(tri_sum)
+    tri_sum_divs = get_divisors2(tri_sum)
+    if len(tri_sum_divs) > 300:
+        print "Close: %dth triangle number, %d, has length: %d" % (i, tri_sum, len(tri_sum_divs))
     if len(tri_sum_divs) > 500:
         print i
         break
 
-print tri_sum,len(tri_sum_divs), tri_sum_divs
+print tri_sum, len(tri_sum_divs), tri_sum_divs
 
 
